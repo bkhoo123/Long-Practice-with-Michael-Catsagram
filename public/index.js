e3c8490b8831ab7f04e051a42c522afd9f42a323
bkhoo123 <102838003+bkhoo123@users.noreply.github.com>
2022-10-25 16:03:23 -0700

    Finished 2 Buttons

--- public/index.js
+++ public/index.js
@@ -1,38 +1,65 @@
-// Your code here
+//! Might Need Async on this
 
-// async function () => {
-
-// }
-
-
-// fetch("https://thecatapi.com", {
-//     method: "GET",
-//     headers: {"Content-Type": "application/json"},
-// })
+const ul = document.createElement("ul")
+ul.id = "comment-Body"
+document.body.appendChild(ul)
 
 
-const fetchingFunction = async () =>  {
-    const res = await fetch ("https://api.thecatapi.com/v1/images/search")
+
+const addCat = () => {
+    const newCat = document.getElementById("newCat") 
+    newCat.addEventListener("click", async () => {
+    const res = await fetch("https://api.thecatapi.com/v1/images/search")
+    
     const data = await res.json()
-    console.log(data[0])
 
     const url = data[0].url
     let split = url.split("/")
-    let catImage = split[4]
-
-    console.log("cat Image", catImage)
+        let catImage = split[4]
     
-    const body = document.querySelector("body")
+        console.log("cat Image", catImage)
+        
+        const body = document.querySelector("body")
+    
+        const span = document.createElement("span")
+        const img = document.createElement('img')
+        
+    
+        img.setAttribute("src", url)
+        img.id = catImage
+    
+        body.append(img)
+    })
+}
 
-    const span = document.createElement("span")
 
-    img.setAttribute("src", url)
-    img.id = catImage
+//? 1: find user input
+//? 2: Store user input as data? 
+//? 3: append it to paragraph?
 
-} 
+
+
+const addComment = () => {
+    
+    // const userInput = document.getElementById("userInput")
+    const commentBody = document.getElementById("comment-Body")
+    
+        const li = document.createElement("li")
+        
+        li.innerText = userInput.value
+        commentBody.appendChild(li)
+}
+const comment = document.getElementById("comments")
+comment.addEventListener("click", () => {
+    addComment()
+})
+
+
+
 
 window.onload = async () => {
-    fetchingFunction()
+    addCat()
+    
 }
 
 

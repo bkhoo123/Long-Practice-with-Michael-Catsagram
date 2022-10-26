6f3495df611518446d9fb402e1bc12e9f356db7b
Michael Valdez <mikeyvaldez92@yahoo.com>
2022-10-26 17:22:01 -0600

    technically done

--- public/button.js
+++ public/button.js
@@ -1,30 +1,47 @@
 //? Adds a cat in the middle of the page
-export const addCat = () => {
-    const newCat = document.getElementById("newCat") 
-    newCat.addEventListener("click", async () => {
+export const addCat = async () => {
     const res = await fetch("https://api.thecatapi.com/v1/images/search")
-    
+
     const data = await res.json()
 
     const url = data[0].url
     let split = url.split("/")
-        let catImage = split[4]
-    
-        console.log("cat Image", catImage)
-        
-        const body = document.querySelector("body")
-    
-        const img = document.createElement('img')
+    let catImage = split[4]
 
-        const container = document.querySelector(".container")
-        
-        img.style.width = '500px'
-        img.style.height = '500px'
-    
-        img.setAttribute("src", url)
-        img.id = catImage
-        container.appendChild(img)
-        
-    })
+    console.log("cat Image", catImage)
+
+    return url;
 }
 
+let clicks = 0;
+const like = document.getElementById("up-vote");
+const disLike = document.getElementById("down-vote");
+const count = document.getElementById("clicked-count");
+
+export const votes = () => {
+
+    like.addEventListener("click", event => {
+        clicks += 1;
+        count.innerHTML = clicks;
+    });
+
+    disLike.addEventListener("click", event => {
+        clicks -= 1;
+        count.innerHTML = clicks;
+    });
+}
+
+newCat.addEventListener("click", async () => {
+    let catImage = document.getElementById("catImage");
+    catImage.src = await addCat();
+    clicks = 0;
+    count.innerHTML = clicks;
+})
+
+export const erase = () => {
+    const eraser = document.getElementById("delete");
+    eraser.addEventListener("click", event => {
+        const deleteComment = document.querySelector("li")
+        deleteComment.remove();
+    });
+}
--- public/index.js
+++ public/index.js
@@ -1,8 +1,11 @@
-import {createImage} from './catImage.js';
+// import {createImage} from './catImage.js';
 import {addCat} from './button.js'
+import {votes} from './button.js'
+import {erase} from './button.js'
 
 
-const initializePage = () => {
+
+const initializePage = async () => {
     // create a container
     const container = document.createElement('section');
     container.className = 'container';
@@ -11,10 +14,23 @@ const initializePage = () => {
     container.style.alignItems = 'center';
     container.style.gap = '25px';
     // container.style.flexWrap = "wrap"
-  
+
     // container.innerText = 'Cat # 1';
     document.body.appendChild(container);
-  };
+    const img = document.createElement('img');
+    img.style.width = '500px'
+    img.style.height = '500px'
+    img.setAttribute("src", await addCat())
+    img.id = 'catImage'
+    container.appendChild(img)
+
+    const placeHolderText = document.createElement("placeHolder");
+    const clickContainer = document.getElementById("click-container");
+    const commentBody = document.getElementById("comment-Body");
+    placeHolderText.innerText = clickContainer;
+    container.append(clickContainer);
+    container.append(commentBody);
+}
 
 
 //? Comments User Input Adds to the top of the page
@@ -22,16 +38,17 @@ const ul = document.createElement("ul")
 ul.id = "comment-Body"
 document.body.appendChild(ul)
 
-
 const addComment = () => {
-    
-    // const userInput = document.getElementById("userInput")
+
     const commentBody = document.getElementById("comment-Body")
-    
-        const li = document.createElement("li")
-        
-        li.innerText = userInput.value
-        commentBody.appendChild(li)
+
+    const li = document.createElement("li")
+
+    li.innerText = userInput.value
+
+    commentBody.appendChild(li)
+
+    userInput.value = "";
 }
 
 const comment = document.getElementById("comments")
@@ -39,23 +56,13 @@ comment.addEventListener("click", () => {
     addComment()
 })
 
-
-
-
-
-
-
-
-
-
 window.onload = () => {
     initializePage()
-    createImage()
     console.log("this is loading")
     addCat()
-    
+    votes()
+    erase()
 }
 
 
 // window.onload purpose is store multiple single functions
-
